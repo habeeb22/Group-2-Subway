bc1f0baf43f1a569dfb7dd58a2c90fdf78c1e6a5
habeeb22 <habeeb22@users.noreply.github.com>
2019-03-19 08:29:45 +0400

    test2

--- Assets/PlayerMoveScript.cs
+++ Assets/PlayerMoveScript.cs
@@ -6,29 +6,35 @@ public class PlayerMoveScript : MonoBehaviour
 {
     public KeyCode moveL;
     public KeyCode moveR;
-    public float horizVel;
+    public float horizVel = 0;
+    public int lamNum = 2;
+    public string controlLocked = "n";
     public GameObject plane;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 4);
-        if (Input.GetKeyDown(moveL))
+
+        GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 4);
+        if ((Input.GetKeyDown(KeyCode.LeftArrow)) && (lamNum>1) && (controlLocked == "n"))
         {
             horizVel = -2;
             StartCoroutine(stopSlide());
-
+            lamNum -= 1;
+            controlLocked = "y";
         }
-        if (Input.GetKeyDown(moveR))
+        if ((Input.GetKeyDown(KeyCode.RightArrow)) && (lamNum < 3) && (controlLocked == "n"))
         {
             horizVel = 2;
             StartCoroutine(stopSlide());
+            lamNum += 1;
+            controlLocked = "y";
         }
 
     }
@@ -36,11 +42,12 @@ public class PlayerMoveScript : MonoBehaviour
     {
         yield return new WaitForSeconds(.5f);
         horizVel = 0;
+        controlLocked = "n";
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag=="Coin")
+        if (other.tag == "Coin")
         {
             GameManager.coin += 1;
             Destroy(other.gameObject);
