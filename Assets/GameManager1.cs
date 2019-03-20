282e4081a7574b96596113416d03d12c9684a0f3
HiMoOoOo <31377306+HiMoOoOo@users.noreply.github.com>
2019-03-20 15:50:13 +0400

    ibrahim

---
+++ Assets/GameManager1.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager1 : MonoBehaviour
+{
+    public static GameManager1 Instance;
+    public Text scoreText;
+    public Text scoreTextG;
+    public Text bestscoreText;
+
+    bool start;
+    public int score;
+    public static int coin;
+    public Text coinText;
+    public Text coinTextG;
+    public float bestscore;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(this);
+
+        //DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(PlayerPrefs.GetInt("Player"));
+        score = 0;
+        coin = 0;
+        scoreText.text = "score: " + 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //GameObject.FindGameObjectWithTag("GameStarted").GameStart();
+        //if (start == true)
+        //{
+        score += 1;
+        //PlayerPrefs.SetInt("player", score);
+
+        //}
+
+        //GameObject.FindGameObjectWithTag(!"GameStarted").GameStop();
+
+        scoreText.text = "score: " + score;
+        scoreTextG.text = "score: " + score;
+        bestscoreText.text = "BestScore: " + bestscore;
+        coinText.text = "Coin: " + coin;
+        coinTextG.text = "Coin: " + coin;
+
+
+
+
+        if ( PlayerPrefs.GetInt("Player") < score )
+
+        {
+            bestscore = score;
+            bestscoreText.text = "BestScore: " + score;
+            PlayerPrefs.SetInt("Player",score);
+ }
+        else
+            bestscoreText.text = "BestScore: " + PlayerPrefs.GetInt("Player");
+    }
+
+    //public void GameStart()
+    //{
+    //    start = true;
+    //    //score += 1;
+    //}
+    //public void GameStop()
+    //{
+    //    start = false;
+    //    //score += 1;
+    //}
+}
---
+++ Assets/PlayerMoveScript6.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMoveScript6 : MonoBehaviour
+{
+    public KeyCode moveL;
+    public KeyCode moveR;
+    public float horizVel = 0;
+    public int lamNum = 2;
+    public string controlLocked = "n";
+    public GameObject plane;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 4);
+
+        if ((Input.GetKeyDown(KeyCode.LeftArrow)) && (lamNum>1) && (controlLocked == "n"))
+        {
+            horizVel = -2;
+            StartCoroutine(stopSlide());
+            lamNum -= 1;
+            controlLocked = "y";
+        }
+        if ((Input.GetKeyDown(KeyCode.RightArrow)) && (lamNum < 3) && (controlLocked == "n"))
+        {
+            horizVel = 2;
+            StartCoroutine(stopSlide());
+            lamNum += 1;
+            controlLocked = "y";
+        }
+
+        //FindObjectOfType<GameOverx>().EndGame();
+
+    }
+    
+    IEnumerator stopSlide()
+    {
+        yield return new WaitForSeconds(.5f);
+        horizVel = 0;
+        controlLocked = "n";
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Lose")
+        {
+
+            SceneManager.LoadScene("GameOver");
+
+
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Coin")
+        {
+            GameManager1.coin += 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
