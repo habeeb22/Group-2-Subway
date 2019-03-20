f3b872b2158489fd26c371584933789865363313
sawsanalrashdi <47941229+sawsanalrashdi@users.noreply.github.com>
2019-03-20 14:06:46 +0400

    sawsan finall
    sawsan finall

---
+++ Assets/clicksound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class clicksound : MonoBehaviour
+{
+
+    public AudioClip sound;
+    private Button button { get { return GetComponent<Button>(); } }
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
+        button.onClick.AddListener(() => playsound());
+    }
+  
+    void playsound()
+    {
+        source.PlayOneShot(sound);
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
