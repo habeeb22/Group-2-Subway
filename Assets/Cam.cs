58a0d657b20b186f7835fb2831995b6d8f1440d1
habeeb22 <habeeb22@users.noreply.github.com>
2019-03-20 11:59:32 +0400

    test

---
+++ Assets/Cam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    float distance;
+    Vector3 playerPrevPos, playerMoveDir;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+
+        distance = offset.magnitude;
+        playerPrevPos = player.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+
+        playerMoveDir = player.transform.position - playerPrevPos;
+        // playerMoveDir.normalized();
+        transform.position = player.transform.position - playerMoveDir * distance;
+
+        transform.LookAt(player.transform.position);
+
+        playerPrevPos = player.transform.position;
+    }
+}
--- Assets/Respwan.cs
+++ Assets/Respwan.cs
@@ -7,7 +7,7 @@ public class Respwan : MonoBehaviour
     public Transform respwanR;
     public Transform respwanM;
     public Transform respwanL;
-    float time = 1;
+    float time = .4f;
     public GameObject wall1;
     public GameObject wall2;
     public GameObject wall3;
@@ -26,29 +26,81 @@ public class Respwan : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 10);
+
         time -= Time.deltaTime;
         if (time <=0)
         {
-            time = 1;
-            Instantiate(wall1, respwanL.position, transform.rotation);
-            randNumW = Random.Range(0, 2);
-            if (randNumW==0)
+            time = 0.4f;
+           // Instantiate(wall1, respwanL.position, transform.rotation);
+            randNumW = Random.Range(0, 11);
+            if (randNumW <3)
             {
-                randNumR = Random.Range(0, 10);
+                randNumR = Random.Range(0, 11);
+                if (randNumR < 3)
+                {
+                    Instantiate(wall1, respwanR.position, transform.rotation);
+                }
+                if (randNumR > 3 && randNumR <7)
+                {
+                    Instantiate(wall2, respwanR.position, transform.rotation);
+                }
                 if (randNumR > 6)
                 {
-
-
+                    Instantiate(wall3, respwanR.position, transform.rotation);
                 }
 
             }
-            if (randNumR > 6)
+            if (randNumW > 3 && randNumW < 7)
             {
-
-                Instantiate(wall1, respwanL.position, transform.rotation);
-
+                randNumM = Random.Range(0, 11);
+                if (randNumM < 3)
+                {
+                    Instantiate(wall1, respwanM.position, transform.rotation);
+                }
+                if (randNumM > 3 && randNumM < 7)
+                {
+                    Instantiate(wall2, respwanM.position, transform.rotation);
+                }
+                if (randNumM > 6)
+                {
+                    Instantiate(wall3, respwanM.position, transform.rotation);
+                }
 
             }
+            if (randNumW > 6)
+            {
+                randNumL = Random.Range(0, 11);
+                if (randNumL < 3)
+                {
+                    Instantiate(wall1, respwanL.position, transform.rotation);
+                }
+                if (randNumL > 3 && randNumL < 7)
+                {
+                    Instantiate(wall2, respwanL.position, transform.rotation);
+                }
+                if (randNumL > 6)
+                {
+                    Instantiate(wall3, respwanL.position, transform.rotation);
+                }
+
+            }
+
+
+
+
+
+
+
+
+
+            //if (randNumR > 6)
+            //{
+
+            //    Instantiate(wall1, respwanL.position, transform.rotation);
+
+
+            //}
             //if (randNumM == 4 || randNum == 5 || randNum == 6)
             //{
             //    Instantiate(Coins, new Vector3(1, 0.8f, planPosition.position.z + -6f), planPosition.rotation);
@@ -94,6 +146,13 @@ public class Respwan : MonoBehaviour
             //    Instantiate(Coins, new Vector3(-1, 0.8f, planPosition.position.z + 6f), planPosition.rotation);
             //}
 
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Finished")
+        {
+            Destroy(gameObject);
         }
     }
 }
